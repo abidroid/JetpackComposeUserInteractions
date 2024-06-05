c898d1ccdddb65d4079f690a809f17c45dbcc2ed
Muhammad Abid <abidroid1@gmail.com>
2024-06-05 10:24:22 +0500

    layout

--- app/src/main/java/com/example/jetpackcomposeuserinteractions/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeuserinteractions/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeuserinteractions.ui.theme.JetpackComposeUserInteractionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +27,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeUserInteractionsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    MyLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -38,10 +49,28 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MyLayout(name: String, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(color = Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {  }) {
+            Text(text = "Show Toast")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeUserInteractionsTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        MyLayout("Android")
     }
 }
