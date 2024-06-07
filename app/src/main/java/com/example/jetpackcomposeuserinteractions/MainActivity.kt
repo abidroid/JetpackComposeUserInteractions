47acb24e7ecabf79e1e6bb0f97e0557fc57d31f1
Muhammad Abid <abidroid1@gmail.com>
2024-06-07 09:16:47 +0500

    dialog

--- app/src/main/java/com/example/jetpackcomposeuserinteractions/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeuserinteractions/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
@@ -20,6 +21,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -41,18 +43,22 @@ class MainActivity : ComponentActivity() {
                 val snackbarHostState = remember { SnackbarHostState() }
 
                 Scaffold(
-                    snackbarHost = { SnackbarHost(hostState = snackbarHostState){
-                        // for styling snackbar
-                        Snackbar(snackbarData = it,
-                            containerColor = Color.Red,
-                            contentColor = Color.White,
-                            actionColor = Color.White,
-                            dismissActionContentColor = Color.White
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState) {
+                            // for styling snackbar
+                            Snackbar(
+                                snackbarData = it,
+                                containerColor = Color.Red,
+                                contentColor = Color.White,
+                                actionColor = Color.White,
+                                dismissActionContentColor = Color.White
 
                             )
 
-                    } },
-                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
 //                    Greeting(
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
@@ -82,8 +88,10 @@ fun MyLayout(name: String, modifier: Modifier = Modifier, snackbarHostState: Sna
     // Note - This only works inside a Composable function
     val myContext = LocalContext.current
 
-   // val snackbarHostState = remember { SnackbarHostState() }
+    // val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
+
+    val dialogStatus = remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -109,7 +117,7 @@ fun MyLayout(name: String, modifier: Modifier = Modifier, snackbarHostState: Sna
                     actionLabel = "Show Toast",
                     duration = SnackbarDuration.Indefinite,
                     withDismissAction = true
-                    )
+                )
 
                 if (result == SnackbarResult.ActionPerformed) {
                     Toast.makeText(myContext, "Hello, I am a Toast message!", Toast.LENGTH_SHORT).show()
@@ -117,6 +125,38 @@ fun MyLayout(name: String, modifier: Modifier = Modifier, snackbarHostState: Sna
             }
         }) {
             Text(text = "Show Snackbar")
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(onClick = {
+
+            dialogStatus.value = true
+
+
+        }) {
+            Text(text = "Show Dialog")
+        }
+
+        if (dialogStatus.value) {
+            AlertDialog(
+                title = { Text(text = "Title") },
+                text = { Text(text = "Message") },
+                onDismissRequest = { dialogStatus.value = false },
+                confirmButton = {
+                    Button(onClick = {
+                        dialogStatus.value = false
+                    }) {
+                        Text(text = "Confirm")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = {
+                        dialogStatus.value = false
+                    }) {
+                        Text(text = "Dismiss")
+                    }
+                }
+            )
         }
     }
 }
